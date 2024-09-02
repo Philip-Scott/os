03535bf64751025c81d516c2ec9078144a4552fd
Felipe Escoto <felescoto95@hotmail.com>
2024-09-02 10:45:20 -0700

    Add azure-cli

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ RELEASE="$(rpm -E %fedora)"
 # Install Microsoft repos for VSCode
 sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
-rpm-ostree install screen conky pass htop code
+rpm-ostree install screen conky pass htop code azure-cli
 
 
 systemctl enable podman.socket
