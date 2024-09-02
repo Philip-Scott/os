040dbf1c3351cfe917c5c68ef8b71063d288303a
Felipe Escoto <felescoto95@hotmail.com>
2024-09-02 10:09:33 -0700

    Update build.sh - Don't use sudo

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -ouex pipefail
 RELEASE="$(rpm -E %fedora)"
 
 # Install Microsoft repos for VSCode
-sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
 rpm-ostree install screen conky pass htop code
 
